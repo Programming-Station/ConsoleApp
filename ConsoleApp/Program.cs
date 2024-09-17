3c27625b6fbb8bb11e099d2f28557fc9cce177d5
Programming_Station <titwig365@gmail.com>
2024-09-17 18:35:18 +0530

    Task 1 Complited

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -11,6 +11,14 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             // WAP To Print Table using by the user
+            int a;
+            Console.Write("Enter Any Number And Find Table = ");
+            a=Convert.ToInt32(Console.ReadLine());
+            for(int i=1; i<=10; i++)
+            {
+                Console.WriteLine(a*i);
+            }
+            Console.ReadLine();
         }
     }
 }
