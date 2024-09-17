3de473fff03ed94ec730c3fb54910255f0599fba
Programming_Station <titwig365@gmail.com>
2024-09-17 20:21:53 +0530

    Task 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // WAP To Print 1 to 10 Number 
+            for(int i=1; i<=10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
