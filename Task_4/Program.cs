79fb9419628b155f76857162a86075a51d996339
Programming_Station <titwig365@gmail.com>
2024-09-17 20:28:40 +0530

    Task 4

---
+++ Task_4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // WAP to input any number and Find Table
+            int a;
+            Console.Write("Enter Any Number & Find Table = ");
+            a=Convert.ToInt32(Console.ReadLine());
+            for(int i=1; i<=10; i++)
+            {
+                Console.WriteLine(a*i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
